ca95a6eac2cb19d9669776c3b754e6c13f5fc762
Marvin Kudjo <33563441+Marvinkwame@users.noreply.github.com>
2022-04-25 04:53:06 +0000

    Update Meme.js

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -62,7 +62,7 @@ export default function Meme() {
                 <button className="form-button" onClick={getMemeImage}>Get A New Meme Image</button>
             </div>
             <div className="meme">
-                <img src={meme.randomImage} className="meme-image" alt="meme-image" />
+                <img src={meme.randomImage} className="meme-image" alt="my-meme" />
                 <h2 className="meme-text top">{meme.topText}</h2>
                 <h2 className="meme-text bottom">{meme.bottomText}</h2>
             </div>
